3d2756c2bf61c565aeeb8447ebf1b242feadd59e
Iqbal Hassan <107492954+Emiqbal11@users.noreply.github.com>
2022-11-16 10:54:16 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css';
+import { CreateSimpleSwitcher } from './components/Graph';
+
+const App = () => {
+  return (
+    <div>
+
+      <CreateSimpleSwitcher/>
+     
+    </div>
+  )
+}
+
+export default App;
